bafc198e6e88857b1ffb154cdefd2b11d12dbb29
BlackBoyRedHeart <151723486+BlackBoyRed@users.noreply.github.com>
2025-11-11 06:30:07 +0530

    Practical 2

---
+++ R/MathOperation.r
@@ -0,0 +1,18 @@
+x <- 15
+y <- 4
+
+# Arithmetic operations
+sum_val <- x + y
+diff_val <- x - y
+prod_val <- x * y
+div_val <- x / y
+mod_val <- x %% y   # Modulo (remainder)
+exp_val <- x ^ 2    # Exponentiation
+
+# Built-in math functions
+sqrt_val <- sqrt(16)
+log_val <- log(10)
+
+print(paste("Addition:", sum_val))
+print(paste("Modulo:", mod_val))
+print(paste("Square Root of 16:", sqrt_val))
