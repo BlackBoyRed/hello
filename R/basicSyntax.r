032392fe56e2d6c4674ac3bba72ba9a40e31dca0
BlackBoyRedHeart <151723486+BlackBoyRed@users.noreply.github.com>
2025-11-11 06:29:29 +0530

    Practical 1

---
+++ R/basicSyntax.r
@@ -0,0 +1,14 @@
+# Assignment
+my_variable <- "Hello, World!"
+number_var <- 42
+
+# Printing values
+print(my_variable)
+print(number_var)
+
+# Checking data types
+print(class(my_variable))
+print(class(number_var))
+
+# Listing all variables in the environment
+print(ls())
